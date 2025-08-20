3af70c09580abcb31bb250af69e0d2ae2b89ec15
dgjPolyTech <dgjpolytech@gmail.com>
2025-08-20 10:42:13 +0900

    arr 생성

---
+++ src/w0820/arrayTest1.java
@@ -0,0 +1,12 @@
+package w0820;
+
+public class arrayTest1 {
+    public static void main(String[] args) {
+        int[] arr = new int[4];
+
+        arr[0] = 1;
+        arr[1] = 2;
+        arr[2] = 3;
+        arr[3] = 4;
+    }
+}
