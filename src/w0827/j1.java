1773bccfdb2dcd4800a11b3b2914582cb42d9ecd
dgjPolyTech <dgjpolytech@gmail.com>
2025-08-27 09:24:22 +0900

    2중 for문 구구단

---
+++ src/w0827/j1.java
@@ -0,0 +1,22 @@
+package w0827;
+
+public class j1 {
+    public static void main(String[] args) {
+        for(int i=2; i<10; i++){
+            for(int j=1; j<10; j++){
+                System.out.println(i+"*"+j+"="+i*j);
+            }
+            System.out.println("-------");
+        }
+
+//        int[] arr1 = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+//        int[] arr2 = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+//
+//        for(int i=0; i <arr1.length; i++){
+//            for(int j=0; j <arr2.length; j++){
+//                System.out.println((i+1)+"x"+(j+1)+"="+arr1[i] * arr2[j]);
+//            }
+//            System.out.println("---------");
+//        }
+    }
+}
