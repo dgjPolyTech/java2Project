38b70ed60219eb35c35eae7c76ca0d2329d59d7b
dgjPolyTech <dgjpolytech@gmail.com>
2025-11-18 20:06:00 +0900

    자바 drawing 과제

--- src/w1112/homework/MainFrame2.java
+++ src/w1112/homework/MainFrame2.java
@@ -3,42 +3,57 @@ package w1112.homework;
 import centerFrame.CenterFrame;
 
 import javax.swing.*;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class MainFrame2 extends JFrame {
+public class MainFrame2 extends JFrame implements ActionListener {
+    PaintingPanel2 panel;
+    String[] rbStrs = {"선", "사각형", "타원"};
+    JRadioButton[] rbs = new JRadioButton[rbStrs.length];
+
     public MainFrame2(){
-        // --- 기본 포함 문구 ---
-        setTitle("userPaintingTool");
+        JPanel panelNorth = new JPanel();
+        ButtonGroup group = new ButtonGroup();
+
+        for(int i=0; i<rbs.length; i++){
+            rbs[i] = new JRadioButton(rbStrs[i]);
+            group.add(rbs[i]);
+            panelNorth.add(rbs[i]);
+            rbs[i].addActionListener(this);
+        }
+        rbs[0].setSelected(true);
+
+        add(panelNorth, "North");
+
+        panel = new PaintingPanel2("선");
+        add(panel);
+
+        setTitle("단순 그림판");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        CenterFrame cf = new CenterFrame(500, 400);
+
+        CenterFrame cf = new CenterFrame(500, 500);
         cf.centerXY();
         setBounds(cf.getX(), cf.getY(), cf.getFw(), cf.getFh());
         setVisible(true);
-        // --- 기본 포함 문구 ---
     }
 
-    MouseAdapter ma = new MouseAdapter() {
-        @Override
-        public void mouseClicked(MouseEvent e) {
-            super.mouseClicked(e);
+    @Override
+    public void actionPerformed(ActionEvent e){
+        String cmd = e.getActionCommand();
+        switch (cmd) {
+            case "선":
+                PaintingPanel2.polygon = "선";
+                break;
+            case "사각형":
+                PaintingPanel2.polygon = "사각형";
+                break;
+            case "타원":
+                PaintingPanel2.polygon = "타원";
+                break;
         }
-
-        @Override
-        public void mousePressed(MouseEvent e) {
-            super.mousePressed(e);
-        }
-
-        @Override
-        public void mouseReleased(MouseEvent e) {
-            super.mouseReleased(e);
-        }
-    };
-
+    }
 
     public static void main(String[] args){
         new MainFrame2();
     }
-
 }
--- src/w1112/homework/PaintingPanel2.java
+++ src/w1112/homework/PaintingPanel2.java
@@ -1,4 +1,60 @@
 package w1112.homework;
 
-public class PaintingPanel2 {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class PaintingPanel2 extends JPanel implements MouseListener {
+    public static String polygon;
+    int x1, y1, x2, y2;
+
+    public PaintingPanel2(String polygon){
+        this.polygon = polygon;
+        setBackground(Color.YELLOW);
+        addMouseListener(this);
+    }
+
+    @Override
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+
+        if (polygon.equals("선")) {
+            g.setColor(Color.RED);
+            g.drawLine(x1, y1, x2, y2);
+        } else {
+            int startX = Math.min(x1, x2);
+            int startY = Math.min(y1, y2);
+            int width = Math.abs(x1 - x2);
+            int height = Math.abs(y1 - y2);
+
+            if (polygon.equals("사각형")) {
+                g.setColor(Color.MAGENTA);
+                g.fillRect(startX, startY, width, height);
+            } else if (polygon.equals("타원")) {
+                g.setColor(Color.BLUE);
+                g.fillOval(startX, startY, width, height);
+            }
+        }
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        x1 = e.getX();
+        y1 = e.getY();
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        x2 = e.getX();
+        y2 = e.getY();
+        repaint();
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {}
+    @Override
+    public void mouseEntered(MouseEvent e) {}
+    @Override
+    public void mouseExited(MouseEvent e) {}
 }
